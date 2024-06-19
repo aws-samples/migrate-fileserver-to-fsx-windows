06b77eecf2e123990cdd9fd8da7d7828d8b078b5
Peter-John M <140262025+Peter-John-M@users.noreply.github.com>
2024-06-19 15:48:23 +0200

    Update FSxFunctions.psm1

--- FSxFunctions/FSxFunctions.psm1
+++ FSxFunctions/FSxFunctions.psm1
@@ -1,9 +1,5 @@
-
 # Get public and private function definition files.
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-# At some stage we cacn add a private credential folder 
-#$Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
-
 
 # Dot source the files
 Foreach ($import in @($Public)) {
@@ -18,4 +14,3 @@ Foreach ($import in @($Public)) {
       Write-Error -Message "Failed to import function $($import.fullname): $_"
   }
 }
-
