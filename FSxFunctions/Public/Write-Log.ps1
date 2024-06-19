5bd3efb9e4e0996e66740f8e52fbe38e205f8b50
Peter-John M <140262025+Peter-John-M@users.noreply.github.com>
2024-06-19 18:30:29 +0200

    Create Write-Log.ps1

---
+++ FSxFunctions/Public/Write-Log.ps1
@@ -0,0 +1,40 @@
+<#
+# Using the default log location
+Write-Log -Level INFO -Message "This is an informational message"
+
+# Specifying a custom log location
+Write-Log -Level ERROR -Message "This is an error message" -LogLocation "C:\Logs"
+#>
+Function Write-Log {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$False)]
+        [ValidateSet("INFO","WARN","ERROR","FATAL","DEBUG")]
+        [String]
+        $Level = "INFO",
+        [Parameter(Mandatory=$True)]
+        [string]
+        $Message,
+        [Parameter(Mandatory=$False)]
+        [string]
+        $Logfile,
+        [Parameter(Mandatory=$False)]
+        [string]
+        $LogLocation = "$($env:USERPROFILE)\Documents\FsxMigrate"
+    )
+
+    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+    $Line = "$Stamp $Level $Message"
+    $Logfilepath = "$LogLocation\FsxMigrate.log"
+
+    if (!(Test-Path $LogLocation)) {
+        New-Item -ItemType Directory -Force -Path $LogLocation | Out-Null
+    }
+
+    if (Test-Path $Logfilepath) {
+        Add-Content $Logfilepath -Value $Line
+    }
+    else {
+        Write-Output $Line
+    }
+}
