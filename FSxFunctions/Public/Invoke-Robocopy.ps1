7486378d44f579eb06fb547402091cdd088961f2
Peter-John M <140262025+Peter-John-M@users.noreply.github.com>
2024-06-19 19:44:32 +0200

    Update Invoke-Robocopy.ps1

--- FSxFunctions/Public/Invoke-Robocopy.ps1
+++ FSxFunctions/Public/Invoke-Robocopy.ps1
@@ -7,7 +7,6 @@ If it's $true, the function proceeds to mount the FSx drive and execute the Robo
 If $UseRoboCopyForDataTransfer is $false, the function logs a message and skips the Robocopy data transfer.
 Invoke-Robocopy -SourceFolder $SourceFolder -FSxDriveLetter $FSxDriveLetter -FSxDNSName $FSxDNSName -LogLocation $LogLocation -UseRoboCopyForDataTransfer $UseRoboCopyForDataTransfer
 #>
-
 function Invoke-Robocopy {
     [CmdletBinding()]
     param (
@@ -22,18 +21,15 @@ function Invoke-Robocopy {
         [Parameter(Mandatory = $true)]
         [bool]$UseRoboCopyForDataTransfer
     )
-
-    if ($UseRoboCopyForDataTransfer) {
+    . $PSScriptRoot\Write-Log.ps1
+    if ($UseRoboCopyForDataTransfer) 
+    {
         # Mount FSx as a PSDrive so we can copy the file server data to FSx
         New-PSDrive -Name $FSxDriveLetter.TrimEnd(':') -PSProvider FileSystem -Root "\\$FSxDNSName\D$" -Persist
-
         foreach ($sourceFolder in $SourceFolder) {
             # Copy the root folder from the Source File Server to FSx D:
             robocopy $sourceFolder "$($FSxDriveLetter.TrimEnd(':'))\" /copy:DATSOU /secfix /e /b /MT:32 /XD '$RECYCLE.BIN' "System Volume Information" /V /TEE /LOG+:"$LogLocation"
         }
-
-        # Disconnect the PSDrive
-        Remove-PSDrive -Name $FSxDriveLetter.TrimEnd(':')
     }
     else {
         Write-Log -Level INFO -Message "UseRoboCopyForDataTransfer is set to false, skipping Robocopy data transfer."
