404415eab7d6c85ce684360499890d298b99517f
Peter-John M <140262025+Peter-John-M@users.noreply.github.com>
2024-08-11 13:46:59 +0200

    Update 2-Robocopy.ps1

--- 2-Robocopy.ps1
+++ 2-Robocopy.ps1
@@ -19,25 +19,25 @@
 #########################################################################
 # COPY DATA TO FSx USING ROBOCOPY
 #########################################################################
-# Mount FSx as a drive letter so we can copy the file server data to FSx 
 Write-Host "Creating drive letter mapping" -ForegroundColor Green
 New-PSDrive -Name $FSxDriveLetter.TrimEnd(':') -PSProvider FileSystem -Root "\\$FSxDNSName\D$" -Persist
+
 # Default log location is C:\RoboCopy.log
 $logFilePath = Join-Path -Path $LogLocation -ChildPath "Robocopy.log"
 
-$isShared = Get-SmbShare -Path $ShareRootFolder -ErrorAction SilentlyContinue
-if ($isShared) {
-    # Top-level folder is shared on the network, copy the entire folder structure
-    # By removing the trailing backslash from the destination path ($FSxDriveLetter), the Robocopy command will copy the entire $SourceFolder directory structure, 
-    # including the top-level folder, to the specified destination.
-    Write-Host "Copying shared top-level folder: $ShareRootFolder" -ForegroundColor Green
-    robocopy $ShareRootFolder $FSxDriveLetter /copy:DATSOU /secfix /e /b /MT:32 /XD '$RECYCLE.BIN' "System Volume Information" /V /TEE /LOG+:"$logFilePath"
-}else{
-    foreach ($SourceFolder in $ShareRootFolder) {
-        # Check if the $SourceFolder is shared on the network
+# $ShareRootFolder is an array of strings
+foreach ($SourceFolder in $ShareRootFolder) {
+    # Check if the $SourceFolder is shared on the network
+    $isShared = Get-SmbShare -Path $SourceFolder -ErrorAction SilentlyContinue
+    if ($isShared) {
+        # Top-level folder is shared on the network, copy the entire folder structure
+        # By removing the trailing backslash from the destination path ($FSxDriveLetter), the Robocopy command will copy the entire $SourceFolder directory structure, 
+        # including the top-level folder, to the specified destination.
+        Write-Host "Copying shared top-level folder: $SourceFolder" -ForegroundColor Green
+        robocopy $SourceFolder $FSxDriveLetter /copy:DATSOU /secfix /e /b /MT:32 /XD '$RECYCLE.BIN' "System Volume Information" /V /TEE /LOG+:"$logFilePath"
+    } else {
         # Top-level folder is not shared on the network, copy the subfolders only
         Write-Host "Copying subfolders of non-shared top-level folder: $SourceFolder" -ForegroundColor Green
-        robocopy "$SourceFolder" $FSxDriveLetter\ /copy:DATSOU /secfix /e /b /MT:32 /XD '$RECYCLE.BIN' "System Volume Information" /V /TEE /LOG+:"$logFilePath"
+        robocopy $SourceFolder $FSxDriveLetter\ /copy:DATSOU /secfix /e /b /MT:32 /XD '$RECYCLE.BIN' "System Volume Information" /V /TEE /LOG+:"$logFilePath"
     }
-
 }
