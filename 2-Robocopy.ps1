3aa8764d635e2e4ee45c41edb82f1209809b47a7
Peter-John M <140262025+Peter-John-M@users.noreply.github.com>
2024-08-11 13:32:45 +0200

    Update 2-Robocopy.ps1

--- 2-Robocopy.ps1
+++ 2-Robocopy.ps1
@@ -1,9 +1,20 @@
 <#
-The script is designed to copy data from the source file server to the FSx (Amazon FSx for Windows File Server) instance using the Robocopy tool.
-It maps the FSx drive letter and then copies the data from the $ShareRootFolder to the FSx drive using the Robocopy command.
-The Robocopy command uses various parameters to ensure a robust and efficient data transfer, such as copy:DATSOU to copy data, timestamps, and security, /secfix to fix security,
-/e to copy subdirectories, /b to copy files in Backup mode, /MT:32 to use 32 multithreaded copies, 
-/XD to exclude specific directories, and /V /TEE /LOG+ to provide verbose output, display progress, and append the log to a file.
+    The script is designed to copy data from the source file server to the FSx (Amazon FSx for Windows File Server) instance using the Robocopy tool.
+    It maps the FSx drive letter and then copies the data from the $ShareRootFolder to the FSx drive using the Robocopy command.
+    The Robocopy command uses various parameters to ensure a robust and efficient data transfer, such as copy:DATSOU to copy data, timestamps, and security, /secfix to fix security,
+    /e to copy subdirectories, /b to copy files in Backup mode, /MT:32 to use 32 multithreaded copies, 
+    /XD to exclude specific directories, and /V /TEE /LOG+ to provide verbose output, display progress, and append the log to a file.
+
+    /copy:DATSOU: Copies data, attributes, timestamps, and security.
+    /secfix: Fixes file and directory security on all copied files.
+    /e: Copies subdirectories, including empty ones.
+    /b: Copies files in Backup mode.
+    /MT:32: Uses 32 multithreaded copies.
+    /XD: Excludes specific directories, in this case, '$RECYCLE.BIN' and 'System Volume Information'.
+    /V: Provides verbose output.
+    /TEE: Displays the Robocopy output to the console.
+    /LOG+: Appends the Robocopy log to the specified file.
+
 #>
 #########################################################################
 # COPY DATA TO FSx USING ROBOCOPY
@@ -11,10 +22,22 @@ The Robocopy command uses various parameters to ensure a robust and efficient da
 # Mount FSx as a drive letter so we can copy the file server data to FSx 
 Write-Host "Creating drive letter mapping" -ForegroundColor Green
 New-PSDrive -Name $FSxDriveLetter.TrimEnd(':') -PSProvider FileSystem -Root "\\$FSxDNSName\D$" -Persist
-    # If $ShareRootFolder = "C:\share1","D:\" The script will loop through each top level folder "C:\share1","D:\" and use robocopy to copy all subfolders located inside share1 and D:\
-    # Default log location is C:\RoboCopy.log
-    $logFilePath = Join-Path -Path $LogLocation -ChildPath "Robocopy.log"
-    foreach ($SourceFolder in $ShareRootFolder){
-        robocopy $SourceFolder $FSxDriveLetter\ /copy:DATSOU /secfix /e /b /MT:32 /XD '$RECYCLE.BIN' "System Volume Information" /V /TEE /LOG+:"$logFilePath"
-    }  
+# Default log location is C:\RoboCopy.log
+$logFilePath = Join-Path -Path $LogLocation -ChildPath "Robocopy.log"
 
+$isShared = Get-SmbShare -Path $ShareRootFolder -ErrorAction SilentlyContinue
+if ($isShared) {
+    # Top-level folder is shared on the network, copy the entire folder structure
+    # By removing the trailing backslash from the destination path ($FSxDriveLetter), the Robocopy command will copy the entire $SourceFolder directory structure, 
+    # including the top-level folder, to the specified destination.
+    Write-Host "Copying shared top-level folder: $ShareRootFolder" -ForegroundColor Green
+    robocopy $ShareRootFolder $FSxDriveLetter /copy:DATSOU /secfix /e /b /MT:32 /XD '$RECYCLE.BIN' "System Volume Information" /V /TEE /LOG+:"$logFilePath"
+}else{
+    foreach ($SourceFolder in $ShareRootFolder) {
+        # Check if the $SourceFolder is shared on the network
+        # Top-level folder is not shared on the network, copy the subfolders only
+        Write-Host "Copying subfolders of non-shared top-level folder: $SourceFolder" -ForegroundColor Green
+        robocopy "$SourceFolder" $FSxDriveLetter\ /copy:DATSOU /secfix /e /b /MT:32 /XD '$RECYCLE.BIN' "System Volume Information" /V /TEE /LOG+:"$logFilePath"
+    }
+
+}
