e9411af206060053992e73bb43b029329c176194
Peter-John M <140262025+Peter-John-M@users.noreply.github.com>
2024-08-11 17:54:40 +0200

    Update MigrationParameters.ps1

--- MigrationParameters.ps1
+++ MigrationParameters.ps1
@@ -2,31 +2,30 @@
 # EDIT THE FOLLOWING VARIABLES:
 ###############################
 $DomainName = (Get-CimInstance -Class Win32_ComputerSystem -ComputerName $env:computername).Domain
-# The DNS name of your destination FSx for Windows file system. amznfsxhkxyen.corp.example.com
-$FSxDNSName = ""
+$FSxDriveLetter = Read-Host -Prompt "Enter the drive letter to use for the Amazon FSx file system (Default: Z:)"
+if ([string]::IsNullOrEmpty($FSxDriveLetter)) {
+    $FSxDriveLetter = "Z:"
+}
 
-# Drive letter will be used to mount FSx on this file server
-$FSxDriveLetter = "Z:" 
+$LogLocation = Read-Host -Prompt "Enter the log file location (Default: C:)"
+if ([string]::IsNullOrEmpty($LogLocation)) {
+    $LogLocation = "C:"
+}
 
-# The FQDN of the endpoint “amznfsxzzzzzzzz.corp.example.com“ Amazon FSx CLI for remote management on PowerShell enables file system administration for users in the file system administrators group
-$FSxDestRPSEndpoint = ""
+$LocalAdminGroup = Read-Host -Prompt "Enter the local admin group (Default: BUILTIN\Administrators)"
+if ([string]::IsNullOrEmpty($LocalAdminGroup)) {
+    $LocalAdminGroup = "BUILTIN\Administrators"
+}
 
-# Alias uses a CNAME record to make things easiier for clients to remember i.e. \\fs1.mytestdomain.local instead of \\EC2AMAZ43567.mytestdomain.local
-# Can be comma separated list enclosed in "fs1.dom.local","fs2.dom.local"
-$Alias = "fs1.$DomainName" # must be in FQDN format and not the hostname of source file server
+$DomainAdminGroup = Read-Host -Prompt "Enter the domain admin group (Default: AWS Delegated Administrators)"
+if ([string]::IsNullOrEmpty($DomainAdminGroup)) {
+    $DomainAdminGroup = "AWS Delegated Administrators"
+}
 
-$LogLocation = "C:" # Log file and share config export location, do not include the \ if C:\ just put C:
-
-# In the context of the file server migration, the script 1-CheckPermissions.ps1 checks if the built in local Administrators group "BUILTIN\Administrators" has been granted permissions on the shared folders. 
-# This is important because local groups, such as the Administrators group, will not have the same level of access on the Amazon FSx for Windows, which is managed by domain-level groups.
-# You can change the $LocalAdminGroup value here and run 1-CheckPermissions.ps1 again if your local admin group is not using the default name "BUILTIN\Administrators".
-$LocalAdminGroup = "BUILTIN\Administrators"
-
-# Replace with the Name of your domain administrators group AWS Managed AD is "AWS Delegated Administrators" and Self Managed is "Domain Admins" 
-$DomainAdminGroup = "AWS Delegated Administrators" 
-
-# $ShareRootFolder can be one or more locations, each location must be enclosed in double quotes "", for example if you have two locations use this format: "C:\share1","D:\" 
-$ShareRootFolder = "C:\share1","D:\" 
+$ShareRootFolder = Read-Host -Prompt "Enter the share root folder(s) (Default: C:\share1,D:\)"
+if ([string]::IsNullOrEmpty($ShareRootFolder)) {
+    $ShareRootFolder = "C:\share1","D:\"
+}
 
 # If $ShareRootFolder = "C:\share1","D:\" The script will robocopy top level folder "C:\share1","D:\" and all subfolders located inside share1 and D:\
 # https://andys-tech.blog/2020/07/robocopy-is-mt-with-more-threads-faster/ 
@@ -34,6 +33,45 @@ $ShareRootFolder = "C:\share1","D:\"
 # Getting Hostname of file server - no need to edit this
 $FQDN = (Resolve-DnsName $(hostname) -Type A).Name
 
+###############################
+# RETRIEVE VALUES AUTOMATICALLY
+###############################
+
+# Retrieve the Amazon FSx file system details
+try {
+    $FSxFileSystem = Get-FsxFileSystem -FileSystemId "<your-file-system-id>" -ErrorAction Stop
+    
+    # Get the DNS name of the Amazon FSx file system
+    $FSxDNSName = $FSxFileSystem.DNSName
+    
+    # Get the Remote PowerShell endpoint for the Amazon FSx file system
+    $FSxDestRPSEndpoint = $FSxFileSystem.WindowsConfiguration.RemoteAdministrationEndpoint
+
+    # Get Alias
+    $Alias = $FSxFileSystem.WindowsConfiguration.Aliases.Name
+    
+    Write-Host "Values retrieved automatically:" -ForegroundColor Green
+    Write-Host "FSxDNSName: $FSxDNSName"
+    Write-Host "FSxDestRPSEndpoint: $FSxDestRPSEndpoint"
+    Write-Host "Alias: $Alias"
+}
+catch {
+    Write-Host "Unable to retrieve values automatically. Please provide the following information manually:" -ForegroundColor Yellow
+
+    # Prompt the user to enter the values with default options
+    $FSxDNSName = Read-Host -Prompt "Enter the DNS name of the Amazon FSx file system (Default: $($FSxFileSystem.DNSName))"
+    if ([string]::IsNullOrEmpty($FSxDNSName)) {
+        $FSxDNSName = $FSxFileSystem.DNSName
+    }
+
+    $FSxDestRPSEndpoint = Read-Host -Prompt "Enter the Remote PowerShell endpoint for the Amazon FSx file system (Default: $($FSxFileSystem.WindowsConfiguration.RemoteAdministrationEndpoint))"
+    if ([string]::IsNullOrEmpty($FSxDestRPSEndpoint)) {
+        $FSxDestRPSEndpoint = $FSxFileSystem.WindowsConfiguration.RemoteAdministrationEndpoint
+    }
+
+    $Alias = Read-Host -Prompt "Enter the alias for the file server (Default: fs1.$DomainName)"
+    
+}
 #####################################################
 # DO NOT EDIT AFTER THIS LINE
 #####################################################
