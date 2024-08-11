4bf0a6f4afcf89081982e925395b25c1bbb788a5
Peter-John M <140262025+Peter-John-M@users.noreply.github.com>
2024-08-11 18:47:29 +0200

    Update 1-CheckPermissions.ps1

--- 1-CheckPermissions.ps1
+++ 1-CheckPermissions.ps1
@@ -1,48 +1,9 @@
-<#
-The script is designed to check the NTFS and SMB share permissions on the source file server.
-It first installs the necessary RSAT (Remote Server Administration Tools) features.
-It defines a Write-Log function to log messages to a log file.
-The script checks if the $ShareRootFolder variable is set and exits with an error message if it's not.
-It exports the current SMB shares to an XML file as a backup.
-The script then loops through each share folder and checks the NTFS permissions. 
-If the local administrators group is found, it prompts the user to add the domain administrators group with full control permissions.
-It also checks the SMB share permissions and prompts the user to remove the local administrators group and add the domain administrators group if found.
-The script provides clear and informative error messages and logs the actions taken.
-#>
-#########################################################################
+ #########################################################################
 # CHECK LOCAL FOLDER PERMISSIONS ON SOURCE FILE SERVER
 #########################################################################
 # Install Prerequisites
 Install-WindowsFeature RSAT-AD-PowerShell,RSAT-ADDS-Tools,RSAT-DNS-Server
-## Log function to write to log file
-Function Write-Log {
-  [CmdletBinding()]
-  Param(
-  [Parameter(Mandatory=$False)]
-  [ValidateSet("INFO","WARN","ERROR","FATAL","DEBUG")]
-  [String]
-  $Level = "INFO",
-  [Parameter(Mandatory=$True)]
-  [string]
-  $Message,
-  [Parameter(Mandatory=$False)]
-  [string]
-  $Logfile
-  )
-  $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
-  $Line = "$Stamp $Level $Message"
-  $Logfilepath = "$LogLocation\FsxMigrate.log"
-  if (!(Test-path $Logfilepath)){
-    New-Item $Logfilepath
-  }
-  If($Logfilepath) {
-      Add-Content $Logfilepath -Value $Line
-  }
-  Else {
-      Write-Output $Line
-  }
 
-}
 # Check ShareRoot not null
 if ($null -eq $ShareRootFolder -or "" -eq $ShareRootFolder ){
     Write-Log -Level ERROR -Message "No ShareRoot specified, please edit the variables section at the top of the script"
@@ -55,15 +16,15 @@ $ShareFolder= Get-SmbShare -Special $false | ? {$_.Name -cnotmatch '^(ADMIN|IPC|
 if (Test-path $LogLocation\SmbShares.xml){
     $Now = (Get-Date).ToString("MMddyyhhmmss")
     Rename-Item -Path $LogLocation\SmbShares.xml -NewName $Now-oldSmbShares.xml
-    
 }
-$ShareFolder| Export-Clixml -Path $LogLocation\SmbShares.xml
+$ShareFolder | Export-Clixml -Path $LogLocation\SmbShares.xml
 
 # Validates mandatory parameters for null and empty values
 if ( ($ShareFolder -eq $null) -or ($ShareFolder.Count -eq 0) ){
     Write-Log -Level ERROR -Message "Mandatory input parameters should not be null or empty."
     exit 1
 }
+
 
 # NTFS PERMISSION CHECK: Get ACLs on each share folder and stop if local admin group is found
 foreach ($share in $ShareFolder)
@@ -73,44 +34,53 @@ foreach ($share in $ShareFolder)
     # Get the current folder NTFS permissions
     $GetAccess = (Get-ACL -Path $share.path).Access
     # Check if local administrators group exists on folder permissions
-    if ( ($GetAccess.IdentityReference -contains "BUILTIN\Administrators") -and ($GetAccess.IdentityReference -notcontains "$DomainAdminGroup") )
+    if ( ($GetAccess.IdentityReference -contains "BUILTIN\Administrators") )
     {
-        Write-Host "ERROR: Found local admin group on $($share.Path) please modify permissions to use domain group as local groups will not have access on FSx" -ForeGroundColor Red
-        Write-Log -Level ERROR -Message "Found local admin group on $($share.Path) please modify permissions to use domain group as local groups will not have access on FSx"
-        Write-Host $($GetAccess.IdentityReference) -ForeGroundColor Green
-        Write-Log -Level ERROR -Message "$($GetAccess.IdentityReference)"
-        # Ask customer if they would like to add the correct NTFS permissions to the folder.
-        $FixPermissions = Read-Host -Prompt 'Would you like to ADD NTFS domain group permission? Insert Yes or No?'
-        if ($FixPermissions -cmatch "^(?i)y(?:es)?$") 
+        # Check if DomainAdminGroup also exists
+        if ($GetAccess.IdentityReference -contains "$NetBIOS\$DomainAdminGroup")
         {
-            $ACLPath = "$($share.Path)"
-            $Identity = "$DomainAdminGroup"
-            $FileSystemRight = "FullControl"
-            $Propagation = "0" # 0 None Specifies that no inheritance flags are set.
-            $inheritance = "3" # 3 The ACE is inherited by child container objects.
-            $RuleType = "Allow"
-            Try {
-                $ACL = Get-Acl -Path $ACLPath
-                $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($Identity,$FileSystemRight,$inheritance,$Propagation,$RuleType)
-                $ACL.SetAccessRule($AccessRule)
-                $ACL | Set-Acl -Path $ACLPath
-            }
-            Catch {
-                $ErrorMsg = $_.Exception.Message
-                Write-Log -Level ERROR -Message "Set folder permissions error: $ErrorMsg"
-                Write-Host "Set folder permissions error: $ErrorMsg" -ForeGroundColor Red
-            }
+            Write-Output "Local Admin Group and $DomainAdminGroup both found on $($share.Path). No need to modify permissions."
+            Write-Log -Level INFO -Message "Local Admin Group and $DomainAdminGroup both found on $($share.Path). No need to modify permissions."
         }
         else
         {
-            Write-Host "Please ensure that you have at least one DOMAIN Group with NTFS permissions added to share folders!" -ForeGroundColor Red
-        } 
-
+            Write-Host "ERROR: Found local admin group on $($share.Path) please modify permissions to use domain group as local groups will not have access on FSx" -ForeGroundColor Red
+            Write-Log -Level ERROR -Message "Found local admin group on $($share.Path) please modify permissions to use domain group as local groups will not have access on FSx"
+            Write-Host $($GetAccess.IdentityReference) -ForeGroundColor Green
+            Write-Log -Level ERROR -Message "$($GetAccess.IdentityReference)"
+            # Ask customer if they would like to add the correct NTFS permissions to the folder.
+            $FixPermissions = Read-Host -Prompt 'Would you like to ADD NTFS domain group permission? Insert Yes or No?'
+            if ($FixPermissions -cmatch "^(?i)y(?:es)?$") 
+            {
+                $ACLPath = "$($share.Path)"
+                $Identity = $DomainAdminGroup
+                $FileSystemRight = "FullControl"
+                $Propagation = "0" # 0 None Specifies that no inheritance flags are set.
+                $inheritance = "3" # 3 The ACE is inherited by child container objects.
+                $RuleType = "Allow"
+                Try {
+                    $ACL = Get-Acl -Path $ACLPath
+                    $AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($Identity,$FileSystemRight,$inheritance,$Propagation,$RuleType)
+                    $ACL.SetAccessRule($AccessRule)
+                    $ACL | Set-Acl -Path $ACLPath
+                }
+                Catch {
+                    $ErrorMsg = $_.Exception.Message
+                    Write-Log -Level ERROR -Message "Set folder permissions error: $ErrorMsg"
+                    Write-Host "Set folder permissions error: $ErrorMsg" -ForeGroundColor Red
+                }
+            }
+            else
+            {
+                Write-Host "Please ensure that you have at least one DOMAIN Group with NTFS permissions added to share folders!" -ForeGroundColor Red
+            } 
+        }
     }else
     {
         Write-Output "Local Admin Group check passed! You can proceed to use AWS DataSync or Robocopy to migrate data to FSx"
         Write-Log -Level INFO -Message "Local Admin Group check passed! You can proceed to use AWS DataSync or Robocopy to migrate data to FSx"
     }
+
     # Get the current share permissions
     $SharePermissions = (Get-SmbShareAccess -Name $share.Name)
 
@@ -131,14 +101,12 @@ foreach ($share in $ShareFolder)
             
             # Write to log
             Write-Log -Level INFO -Message "The local administrators group has been replaced with the domain administrators group in the share permissions."
-            Write-Output "The $($DomainAdminGroup) group has been added to the share permissions."
+            Write-Output "The $DomainAdminGroup group has been added to the share permissions."
         }
         else
         {
             Write-Output "Please remove local admin group from SMB permission manually to continue"
             Write-Log -Level ERROR -Message "Please remove local admin group from SMB permission manually to continue"
         }
-    
-    } 
-
-}
+    }
+} 
