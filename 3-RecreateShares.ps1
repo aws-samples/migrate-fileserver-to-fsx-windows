fa1210adfc55822b63027cbbda38590f2fc0d6ec
Peter-John M <140262025+Peter-John-M@users.noreply.github.com>
2024-09-29 19:01:05 +0200

    Update 3-RecreateShares.ps1

--- 3-RecreateShares.ps1
+++ 3-RecreateShares.ps1
@@ -1,4 +1,4 @@
-<#
+ <#
 Validate PowerShell Endpoint Connectivity: The script checks if the PowerShell endpoint for the destination FSx file system is reachable. If not, it logs an error and exits.
 
 Import Shares from XML: The script imports the list of shares from the XML file generated in the previous script.
@@ -6,14 +6,10 @@ Import Shares from XML: The script imports the list of shares from the XML file 
 Prepare Share Creation Parameters: The script filters the share properties to only include the parameters that are accepted by the New-FSxSmbShare cmdlet.
 
 Recreate Shares on FSx: The script loops through each share and creates a new share on the destination FSx file system using the Invoke-Command cmdlet and the New-FSxSmbShare cmdlet. 
-It constructs the share path on the FSx file system based on the share name.
-    If the share already exists on the FSx file system, the script skips it and logs a message.
-    If the share does not exist, the script creates a new share and logs a message.
 
+It constructs the share path on the FSx file system based on the share name.
 #>
-#########################################################################
-# RECREATE FILE SHARES ON FSX USING POWERSHELL REMOTING
-#########################################################################
+
 ## Validate if PowerShell Endpoints are reachable
 $TestDestPS = (Invoke-Command -ConfigurationName FSxRemoteAdmin -ComputerName $FSxDestRPSEndpoint -Credential $FSxAdminUserCredential -ErrorAction Stop -ScriptBlock {Get-FSXSmbShare})
 if (!($TestDestPS)){
@@ -40,24 +36,20 @@ foreach ($item in $shares) {
 
     Try
     {
-        $SharePath = "D:\$($item.Path.Substring($item.Path.LastIndexOf('\') + 1))"
-        # Construct the FSx-based share path 
-        $SharePath = "D:\$shareName"
-        # [System.IO.Path] is a .NET class that provides methods for working with file and directory paths.
-        # For example, if $item.Path is "C:\share1\Finance", then [System.IO.Path]::GetFileName($item.Path) would return "Finance" so we can recreate the share on FSx D:\Finance
-        $shareName = [System.IO.Path]::GetFileName($item.Path) 
-        if ($TestDestPS.Name -match $item.Name){
-            Write-Output "Share already exists, skipping"
-            Write-Log -Level INFO -Message "Share already exists, skipping"
-        }else{
-            Write-Output "About to create a share called $($item.Name)"
-            Write-Log -Level INFO -Message "About to create a share called $($item.Name)"
-            $CreateShare = Invoke-Command -ConfigurationName FSxRemoteAdmin -ComputerName $FSxDestRPSEndpoint -ErrorVariable errmsg -ScriptBlock {New-FSxSmbShare -Path $Using:SharePath -Credential $Using:FSxAdminUserCredential @Using:param}
-            Write-Output $CreateShare
+        Write-Output "About to create a share called $($item.Name)"
+        Write-Log -Level INFO -Message "About to create a share called $($item.Name)"
+        if ($item.Path -like 'C:\*') {
+         $SharePath = 'D:' + $item.Path.Substring(2)
+        } else {
+            $SharePath = $item.Path
         }
+        $CreateShare = Invoke-Command -ConfigurationName FSxRemoteAdmin -ComputerName $FSxDestRPSEndpoint -ErrorVariable errmsg -ScriptBlock {New-FSxSmbShare -Path $Using:SharePath -Credential $Using:FSxAdminUserCredential @Using:param}
+        Write-Output $CreateShare
+       
     }
     Catch
     {
         Write-Log -Level ERROR -Message $_
+        Write-Output $_
     }
 }
